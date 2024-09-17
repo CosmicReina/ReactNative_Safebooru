8574ed85c09b22e56cdf6811f642e838ef016b26
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-18 00:03:22 +0900

    Update FetchSafebooru.js

--- scripts/FetchSafebooru.js
+++ scripts/FetchSafebooru.js
@@ -1,7 +1,53 @@
-async function fetchMaxPID() {
+async function fetchSafebooru(tags) {
+
+
+    tags = tags !== "" ? processTags(tags) : "all";
+
+    let maxPID;
+    if (tags !== "") {
+        maxPID = await fetchMaxPID(tags);
+        maxPID = maxPID > 200000 ? 200000 : maxPID;
+    } else {
+        maxPID = 200000;
+    }
+
+    const step = 42;
+    const randomMax = Math.floor(maxPID / step);
+    const random = Math.floor(Math.random() * (randomMax + 1));
+    const pid = random * step;
+
+    const path = await fetchRandomPost(tags, pid);
+}
+
+async function fetchRandomPost(tags, pid) {
     const {JSDOM} = require("jsdom");
 
-    const url = "https://safebooru.org/index.php?page=post&s=list&tags=all";
+    const url = "https://safebooru.org/index.php?page=post&s=list"
+        + "&tags=" + tags
+        + "&pid=" + pid;
+
+    const data = await fetch(url);
+    const html = await data.text();
+
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const imageList = document.querySelector(".image-list");
+    const aElements = imageList.querySelectorAll("a");
+
+    const randomMax = aElements.length;
+    const random = Math.floor(Math.random() * (randomMax + 1));
+
+    const randomElement = aElements[random];
+    return randomElement.href;
+}
+
+async function fetchMaxPID(tags) {
+    const {JSDOM} = require("jsdom");
+
+    const url = "https://safebooru.org/index.php?page=post&s=list"
+        + "&tags=" + tags;
+
     const data = await fetch(url);
     const html = await data.text();
 
@@ -14,3 +60,13 @@ async function fetchMaxPID() {
     const split = hrefLast.split("pid=");
     return split[split.length - 1];
 }
+
+function processTags(tags) {
+    return tags.split(" ").join("+");
+}
+
+// fetchSafebooru("akiyama_mizuki").then(() => {});
+
+// fetchRandomPost("akiyama_mizuki+1girl", 100).then(() => {});
+
+// console.log(processTags("akiyama_mizuki 1girl"));
